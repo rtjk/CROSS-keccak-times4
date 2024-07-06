5d73c978b5fc99449ba26195f16ab278b388f628
rtjk <47841774+rtjk@users.noreply.github.com>
2024-07-06 13:14:44 +0200

    forgot to memcpy the salt in seedtree.c with NO_TREES

--- CROSS_submission_package_v1.2/Optimized_Implementation/lib/seedtree.c
+++ CROSS_submission_package_v1.2/Optimized_Implementation/lib/seedtree.c
@@ -104,6 +104,7 @@ int compute_round_seeds(unsigned char rounds_seeds[T*SEED_LENGTH_BYTES],
    /* from the 4 seeds generale all T leaves */
    for (int i = 0; i < 4; i++){
       memcpy(csprng_inputs[i],&quad_seed[i*SEED_LENGTH_BYTES],SEED_LENGTH_BYTES);
+      memcpy(csprng_inputs[i]+SEED_LENGTH_BYTES,salt,SALT_LENGTH_BYTES);
       /* increment the domain separation counter */
       csprng_inputs[i][SEED_LENGTH_BYTES+SALT_LENGTH_BYTES] = 0;
       csprng_inputs[i][SEED_LENGTH_BYTES+SALT_LENGTH_BYTES+1] = i+2;
